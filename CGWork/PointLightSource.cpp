cce354c4932a214e24e73d0712d484622feeee8d
tsachiblau <tsachiblau@gmail.com>
2019-12-31 05:17:20 +0200

    thats it

--- CGWork/PointLightSource.cpp
+++ CGWork/PointLightSource.cpp
@@ -9,15 +9,14 @@ PointLightSource::PointLightSource(Point _location, LightCoefficient _I_p)
 Vector PointLightSource::getNormal(Point Objectlocation)
 {
 	Vector L = Vector(Objectlocation.getX() - location.getX(), Objectlocation.getY() - location.getY(), Objectlocation.getZ() - location.getZ(), 0);
-	double norm = sqrt(pow(L[0],2) + pow(L[1], 2) + pow(L[2], 2));
-	norm = (norm > 0) ? norm : 1;
-	return Vector(L[0] / norm, L[1] / norm, L[2] / norm, 0);
+	L.Normalize();
+	return L;
 }
 
 LightCoefficient PointLightSource::getI_p(Point objectLocation)
 {
 	Vector L = Vector(objectLocation.getX() - location.getX(), objectLocation.getY() - location.getY(), objectLocation.getZ() - location.getZ(), 0);
-	double norm = sqrt(pow(L[0], 2) + pow(L[1], 2) + pow(L[2], 2));
+	double norm = L.abs();
 	double decay = (1 / norm);
 	decay = (decay < 1) ? decay : 1;
 	return I_p * (-log10(decay));
