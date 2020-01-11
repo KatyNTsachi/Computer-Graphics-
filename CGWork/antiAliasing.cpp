0d7321e7891b4fce9c1d2d976dd7abc5162d4c98
katiePodiatchev <katy1427@gmail.com>
2020-01-11 12:27:49 +0200

    kernal_types

--- CGWork/antiAliasing.cpp
+++ CGWork/antiAliasing.cpp
@@ -6,9 +6,24 @@ void antiAliasing::blur(int view_mat[], int width, int height, int kernal_size, 
 
 }
 
-std::vector<double> antiAliasing::getKernal(int kernal_size, int kernal_type)
+std::vector<double> antiAliasing::getKernal(int kernal_size, kernalTypes kernal_type)
 {
-
+	if (kernal_type == BOX)
+	{
+		return getBoxKernalOfSize(kernal_size);
+	}
+	else if (kernal_type == TRIANGLE)
+	{
+		return getTriangleKernalOfSize(kernal_size);
+	}
+	else if (kernal_type == GAUSSIAN)
+	{
+		return getGaussianKernalOfSize(kernal_size);
+	}
+	else // SINC
+	{
+		return getSincKernalOfSize(kernal_size);
+	}
 }
 
 std::vector<double> antiAliasing::getRelevantPixels(int view_mat[], int width, int height, int x, int y, int kernal_size)
@@ -19,4 +34,34 @@ std::vector<double> antiAliasing::getRelevantPixels(int view_mat[], int width, i
 double getMul(std::vector<double> &relevant_pixels, std::vector<double> &kernal)
 {
 
+}
+
+std::vector<double> antiAliasing::getBoxKernalOfSize(int kernal_size)
+{
+	double val = 1.0 / 9;
+	return std::vector<double>(val, val, val, val, val, val, val, val, val);
+	/*
+	if (kernal_size == 3)
+	{
+		double val = 1.0 / 9;
+		return std::vector(val, val, val, val, val, val, val, val, val);
+	}
+	else
+	{
+		double val = 1.0 / 25;
+		return std::vector(val, val, val, val, val, val, val, val, val, );
+	}
+	*/
+}
+std::vector<double> antiAliasing::getTriangleKernalOfSize(int kernal_size)
+{
+	return getBoxKernalOfSize(kernal_size);
+}
+std::vector<double> antiAliasing::getGaussianKernalOfSize(int kernal_size)
+{
+	return getBoxKernalOfSize(kernal_size);
+}
+std::vector<double> antiAliasing::getSincKernalOfSize(int kernal_size)
+{
+	return getBoxKernalOfSize(kernal_size);
 }
--- CGWork/antiAliasing.h
+++ CGWork/antiAliasing.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "stdafx.h"
 #include <vector>
+#include "CGWorkDefines.h"
 
 class antiAliasing
 {
@@ -9,8 +10,12 @@ public:
 	static void blur(int view_mat[], int width,int height,int kernal_size, int kernal_type);
 
 private:
-	static std::vector<double> getKernal(int kernal_size, int kernal_type);
+	static std::vector<double> getKernal(int kernal_size, kernalTypes kernal_type);
 	static std::vector<double> getRelevantPixels(int view_mat[], int width, int height, int x, int y, int kernal_size);
 	static double getMul(std::vector<double> &relevant_pixels, std::vector<double> &kernal);
+	static std::vector<double> getBoxKernalOfSize(int kernal_size);
+	static std::vector<double> getTriangleKernalOfSize(int kernal_size);
+	static std::vector<double> getGaussianKernalOfSize(int kernal_size);
+	static std::vector<double> getSincKernalOfSize(int kernal_size);
 };
 
