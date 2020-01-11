ef4967d388b1bf0ffcae44f79c9971bbb05b764d
tsachiblau <tsachiblau@gmail.com>
2020-01-11 12:06:12 +0200

    tmp

--- CGWork/antiAliasing.cpp
+++ CGWork/antiAliasing.cpp
@@ -13,10 +13,10 @@ std::vector<double> antiAliasing::getKernal(int kernal_size, int kernal_type)
 
 std::vector<double> antiAliasing::getRelevantPixels(int view_mat[], int width, int height, int x, int y, int kernal_size)
 {
-
+	std::vector<double>
 }
 
-double antiAliasing::getMul(vector<double> &relevant_pixels, vector<double> &kernal)
+double getMul(std::vector<double> &relevant_pixels, std::vector<double> &kernal)
 {
 
 }
--- CGWork/antiAliasing.h
+++ CGWork/antiAliasing.h
@@ -11,6 +11,6 @@ public:
 private:
 	static std::vector<double> getKernal(int kernal_size, int kernal_type);
 	static std::vector<double> getRelevantPixels(int view_mat[], int width, int height, int x, int y, int kernal_size);
-	static double getMul(vector<double> &relevant_pixels, vector<double> &kernal);
+	static double getMul(std::vector<double> &relevant_pixels, std::vector<double> &kernal);
 };
 
